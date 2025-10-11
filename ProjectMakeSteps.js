d51fb83887f51f046f62cf00cf3f81f57508f126
emonmahdi <developeremon1997@gmail.com>
2025-10-11 06:53:48 +0600

    connect the tailwind css and daisy ui

---
+++ ProjectMakeSteps.js
@@ -0,0 +1,9 @@
+/* 
+1. Project setup
+2. React router, Tailwind and daisy ui setup 
+3. File and folder structure make and react router structure apply Main layouts
+
+
+
+
+*/
--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,15 @@ function App() {
 
   return (
     <>
-       <h2>Career Hub Application</h2>
+      <h2 className="text-3xl bg-blue-600 p-4 font-bold">
+        Career Hub Application
+      </h2>
+      <div className="bg-white p-6">
+        <button className="btn btn-neutral btn-outline">Outline</button>
+        <button className="btn btn-neutral btn-dash">Dash</button>
+      </div>
     </>
-  )
+  );
 }
 
 export default App
