48dfa7a71647a21254dab906cf2636ec8c70f915
emonmahdi <developeremon1997@gmail.com>
2025-10-11 07:40:22 +0600

    login and regiter component create and route set

--- ProjectMakeSteps.js
+++ ProjectMakeSteps.js
@@ -2,7 +2,8 @@
 1. Project setup
 2. React router, Tailwind and daisy ui setup 
 3. File and folder structure make and react router structure apply Main layouts
-
+4. Navbar and footer component and home component create and apply
+5. Login and Register component and routes file setup react router
 
 
 
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,19 +1,26 @@
-import { Component } from "react";
 import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
 import RootLayout from "../layouts/RootLayout";
-import Home from "../components/Home/Home";
-
+import Home from "../pages/Home/Home";
+import Register from "../pages/Register/Register";
+import Login from "../pages/Login/Login";
 
 export const router = createBrowserRouter([
-    {
-      path: "/",
-      Component:RootLayout,
-      children:[
-        {
-            index:true,
-            Component:Home
-        }
-      ]
-    },
-  ]);
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "/register",
+        Component: Register,
+      },
+      {
+        path: "/login",
+        Component: Login,
+      },
+    ],
+  },
+]);
